cda5de91b1a2aff3cfb04616acc1092b7bbdc3f5
multiahfoon <multiahfoon@gmail.com>
2020-12-16 12:19:53 +1300

    feat: display text if user guess correct

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const userInput = document.getElementsByClassName('userInput')[0];
+const guessResult = document.getElementsByClassName('guessResult')[0];
 
 userInput.focus();
 const secretWord = 'bruh';
@@ -6,9 +7,12 @@ const secretWord = 'bruh';
 
 userInput.addEventListener('keyup', function(event) {
     if(event.key == 'Enter') {
-        if(userInput.value == secretWord) {
-            console.log("You got the secret word right");
+        if(userInput.value !== secretWord) {
+            guessResult.innerText = 'Not quite there. Please Try again';
+        }else {
+            guessResult.innerText = 'You got the secret word right!';
         }
+
         userInput.value = '';
     }    
 });
