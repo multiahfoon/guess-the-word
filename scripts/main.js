2b620debd1b077fcd32de429877b6a4599a42943
multiahfoon <multiahfoon@gmail.com>
2020-12-18 15:26:10 +1300

    feat: display available tries for user

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const userInput = document.getElementById('userInput');
 const guessResult = document.getElementById('guessResult');
 const hintDisplay = document.getElementById('hints');
-
+const tries = document.getElementById('tries');
 userInput.focus();
 
 // an array of words to pick from as secret word
@@ -18,33 +18,31 @@ for(let i = 0; i <= Math.floor(secretWord.length / 3); i++) {
 }
 
 
+let currentUserTry = 7;
 console.log(hintArray);
 
 // display hint text for user
 hintDisplay.innerText = `The word has ${ secretWord.length} letters and starts with "${hintArray.join('')}"`
+tries.innerText = `Current tries left: ${currentUserTry}`
 
-// event listener used to check users guess
-let currentUserTry = 1;
+
 
 // look for another listener because this fires for every button pressed might not be a good thing
 userInput.addEventListener('keyup', function(event) {
-    console.log(currentUserTry);
-    
-    
     if(event.key == 'Enter') {
+        currentUserTry--;
         if(userInput.value !== secretWord) {
             guessResult.innerText = `"${userInput.value}" was not the secret word`;
         }else if(userInput.value == secretWord){
             guessResult.innerText = `You got the secret word "${secretWord}" right!`;
         }
-        if(currentUserTry == 5) {
+        if(currentUserTry < 1) {
             console.log('max tries');
             userInput.style.display = 'none';
         }
 
-        currentUserTry++;
         userInput.value = '';
+        // NOTE: duplicate, this can be refactored
+        tries.innerText = `Current tries left: ${currentUserTry}`
     }
-
 });
-
