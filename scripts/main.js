fac826668b97661387f75982381daf9c60543868
multiahfoon <multiahfoon@gmail.com>
2020-12-21 13:11:23 +1300

    feat: add reset button

--- scripts/main.js
+++ scripts/main.js
@@ -5,27 +5,51 @@ const tries = document.getElementById('tries');
 userInput.focus();
 
 // an array of words to pick from as secret word
-let wordlist = ['green', 'tree', 'winter', 'snow', 'spring', 'fall', 'sunday']
 
 // random number generated and used to select a word from the word list
-let randomNum = Math.floor(Math.random() * wordlist.length);
-let secretWord = wordlist[randomNum];
-
-let hintArray = [];
-console.log(secretWord);
-for(let i = 0; i <= Math.floor(secretWord.length / 3); i++) {
-    hintArray.push(secretWord[i]);
+const generateSecretWord = () => {
+    let wordlist = ['green', 'tree', 'winter', 'snow', 'spring', 'fall', 'sunday']
+    let randomNum = Math.floor(Math.random() * wordlist.length);
+    return wordlist[randomNum];
 }
+let secretWord = generateSecretWord();
 
-
+const generateHint = () => {
+    let arr = [];
+    for(let i = 0; i <= Math.floor(secretWord.length / 3); i++) {
+        arr.push(secretWord[i]);
+    }
+    return arr;
+}
+let hintArray = generateHint();
 let currentUserTry = 7;
+
+console.log(secretWord);
 console.log(hintArray);
 
 // display hint text for user
-hintDisplay.innerText = `The word has ${ secretWord.length} letters and starts with "${hintArray.join('')}"`
-tries.innerText = `Current tries left: ${currentUserTry}`
+const generateGameDisplay = () => {
+    hintDisplay.innerText = `The word has ${ secretWord.length} letters and starts with "${hintArray.join('')}"`;
+    tries.innerText = `Current tries left: ${currentUserTry}`;
+}
+generateGameDisplay();
 
+const resetGame = () => {
+    currentUserTry = 7;
+    guessResult.innerText = '';
+    secretWord = generateSecretWord();
+    hintArray = generateHint();
+    generateGameDisplay();
+    userInput.style.display = 'inline';
 
+}
+
+const newWord = document.getElementById('newWord');
+
+newWord.addEventListener('click', function(){
+    resetGame();
+    console.log(secretWord);
+});
 
 // look for another listener because this fires for every button pressed might not be a good thing
 userInput.addEventListener('keyup', function(event) {
